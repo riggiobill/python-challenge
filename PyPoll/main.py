5feb5c6e84272e329097187877e5940093f436c9
BillRigg <riggiobill@gmail.com>
2020-08-18 23:01:01 -0400

    Main code for Poll
    Finished the biggest chunk for the PyPoll code: the loop that passes through it and generates a dictionary of candidates and votes. With this information readly at hand, the rest is easy and quick.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -31,16 +31,42 @@ poll_csv = os.path.join("Resources","election_data.csv")
 
 
 
-with open(poll_csv) as csvfile:
-    csvreader = csv.reader(csvfile)
-
-    for row in csvreader:
-        print (row)
-
-
-
-
 #list of needed variables :
 #
 # TotalVotes amount, Candidates{} dictionary, with CandName : CandVotes, CandidateAmount for easy looping, HighestAmount & YoureWinner for final results
-#
+
+TotalVotes = 0
+Candidates = dict()
+count = 0
+NumofCandidates = 0
+
+
+
+with open(poll_csv) as csvfile:
+    csvreader = csv.reader(csvfile)
+    cvs_header = next(csvfile)
+    #adjusts past the header row
+  
+
+    for row in csvreader:               #This loop parses through the whole content, with conditions for a new candidate or an existing candidate.
+        TotalVotes = TotalVotes+1       #It then either adds the candidate to the dictionary above or not, and increments the vote either way for both
+        Candidate = row[2]              #total vote as a separate variable or individual vote as part of the dictionary.
+
+        if (Candidate in Candidates) :
+            Candidates[Candidate] += 1
+            
+        else:
+            NumofCandidates += 1
+            Candidates.update({Candidate:0})
+            Candidates[Candidate]+=1
+     
+    print("Total Votes:")
+    print(TotalVotes)
+
+    
+    #     the loop needs to keep track of volume of overall votes and amount of votes for each candidate. Then at the end of the looping it can determine:
+#   A. Who got how many votes each
+#   B. What those percentages were, and
+#   C. Who won the popular vote
+# 
+# Last: winner of the election based on popular vote, from vote count above
